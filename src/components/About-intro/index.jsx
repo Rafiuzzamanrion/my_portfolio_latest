1f302055685cb69831d8ee80ec1413408aa05102
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-10-07 00:02:03 +0600

    added about intro

--- src/components/About-intro/index.jsx
+++ src/components/About-intro/index.jsx
@@ -8,21 +8,14 @@ const AboutIntro = () => {
         <div className="row">
           <div className="col-lg-3 col-md-4">
             <div className="htit sm-mb30">
-              <h4>Who We Are ?</h4>
+              <h4>Who Am I ?</h4>
             </div>
           </div>
           <div className="col-lg-8 offset-lg-1 col-md-8">
             <div className="text">
               <Split>
                 <p className="wow txt words chars splitting" data-splitting>
-                  We develop creative solutions for small and big brands alike,
-                  build authentic product identities and much more.Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit sit non facilisis
-                  vitae eu. Ultrices ut diam morbi risus dui, nec eget at lorem
-                  in id tristique in elementum leo nisi eleifend placerat magna
-                  lacus elementum ornare vehicula odio posuere quisque ultrices
-                  tempus cras id blandit maecenas in ornare quis dolor tempus
-                  risus vitae feugiat fames aliquet sede.
+                  I am a passionate Full Stack Web Developer specializing in building innovative web applications and interactive user experiences. Leveraging modern technologies like React, Next.js, and Node.js, I transform complex ideas into scalable digital solutions. My portfolio showcases a commitment to clean code, performance, and delivering successful projects for clients and businesses. I hold a B.Sc. in Computer Science and Engineering, which provides a strong foundation in both theoretical and practical aspects of software development.
                 </p>
               </Split>
             </div>
