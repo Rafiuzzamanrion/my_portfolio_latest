89740a93b4a3a14146578eac150715a43cebf022
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-10-06 22:41:27 +0600

    added about image header

--- src/components/About-header/index.jsx
+++ src/components/About-header/index.jsx
@@ -4,19 +4,19 @@ const AboutHeader = () => {
   return (
     <header
       className="pages-header bg-img valign parallaxie"
-      style={{ backgroundImage: "url(/img/slid/1.jpg)" }}
+      style={{ backgroundImage: "url(/img/slid/about-log.png)" }}
       data-overlay-dark="5"
     >
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="cont text-center">
-              <h1>About Us</h1>
+              <h1>About Me</h1>
               <div className="path">
                 <a href="#0">Home</a>
                 <span>/</span>
                 <a href="#0" className="active">
-                  About Us
+                  About Me
                 </a>
               </div>
             </div>
