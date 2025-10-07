bb9565a27a3d806540bf0415262e0fe201e48a44
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-10-08 01:18:07 +0600

    completed projects

--- src/components/Works-three-column-with-filter/index.jsx
+++ src/components/Works-three-column-with-filter/index.jsx
@@ -44,7 +44,7 @@ const WorksThreeColumnWithFilter = ({ filterPosition }) => {
             {portfolioData.map((project, index) => (
               <div key={index} className={`col-lg-4 col-md-6 items ${project.category}${project.marginClass ? ' ' + project.marginClass : ''}`}>
                 <div className="item-img wow fadeInUp" data-wow-delay=".4s">
-                  <Link href={project.link}>
+                  <Link href={project.link} target="_blank" rel="noreferrer">
                     <a>
                       <img style={{height:'350px', objectFit:'cover', objectPosition:'right'}} src={project.image} alt="image" />
                     </a>
